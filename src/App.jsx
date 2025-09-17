9b379df411bcc2bb90944ea7c08ba04c5a639c03
Ansh Tyagi <officialanshtyagi112@gmail.com>
2025-09-17 23:23:26 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useRef, useState, useEffect } from 'react'
-import Navbar from './components/Navbar'
+import { useRef, useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-  const [todo, setTodo] = useState('')
-  const [second, setSecond] = useState([])
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
   const [third, setThird] = useState(false)
   const focus = useRef(null)
 
@@ -13,40 +17,35 @@ function App() {
   }, [todo])
 
 
-  const saveToLS = (params) => {
-    localStorage.setItem("second", JSON.stringify(second))
+  const saveToLS = () => {
+    localStorage.setItem("setTodos", JSON.stringify(todos))
   }
 
   useEffect(() => {
-    let getItem = localStorage.setItem("setSecond", second)
-    if (getItem) {
-      let second = Json.parse(localStorage.getItem("second"))
-      setSecond(second)
-    }
+    let todos = JSON.parse(localStorage.getItem("todos"))
+    setTodos(todos)
   }, [])
 
-  const handelClick2 = () => {
-    setSecond([...second, todo])
+  const add = () => {
+    setTodos([...todos, { id: uuidv4(), todo }])
     setTodo("")
     saveToLS()
   }
 
-  const deleteEvent = (e, index) => {
-    let t = second.filter(i => i.index === index)
-    setTodo(t[0].todo)
-    let newSecond = second.filter(item => {
-      return item.index !== index
+  const deleteEvent = (_, id) => {
+    let newtodos = todos.filter(item => {
+      return item.id !== id
     });
-    setSecond(newSecond)
-    saveToLS()
+    setTodos(newtodos)
   }
 
-  const editEvent = (e, index) => {
-    let edit = second.filter(item => {
-      deleteEvent()
-      return item.index === index
+  const editEvent = (_, id) => {
+    let edit = todos.filter(item => {
+      return item.id === id
     })
-    setTodo(edit)
+    deleteEvent(edit, id)
+    setTodo(edit[0].todo)
+    saveToLS()
   }
 
   return (
@@ -57,37 +56,48 @@ function App() {
 
           <div className='button flex justify-between items-center'>
             <input onChange={(e) => setTodo(e.target.value)} value={todo} ref={focus} className='border border-gray-300 rounded-[8px] w-[460px] h-[40px] text-[16px] px-1' type="text" name='text' placeholder='Add a new task...' />
-            <button onClick={handelClick2} className='w-[80px] h-[40px] bg-blue-500 rounded-xl text-white text-bold'>Add</button>
+            <button onClick={add} className='w-[80px] h-[40px] bg-blue-500 rounded-xl text-white text-bold'>Add</button>
           </div>
           <h2 className='font-bold mt-[25px]'>My Todo's</h2>
-          <div className='flex items-center'>
-            <input type="checkbox" name="show" id="show" />
-            <label htmlFor="show" className='ml-1'>Finshed Todos</label>
-          </div>
-
-          <div>
-            <input type="checkbox" name="show" id="shownot" />
-            <label htmlFor="show"></label>
-          </div>
 
           {
-            second.map((item, index) => {
-              return (
-                <div key={index}>
-                  <div className='flex gap-2 items-center'>
-                    <label htmlFor="show"></label>
-                    <input type="checkbox" name="show" id="shownot" />
-                    <div>{item}</div>
-                    <button className='border button bg-gray-200 font-bold p-2 rounded-[10px]' onClick={(e) => editEvent(e, item.index)}>Edit</button>
-                    <button className='border button bg-gray-200 font-bold p-2 rounded-[10px]' onClick={(e) => deleteEvent(e, item.index)}>deleteEvent</button>
-                  </div>
-                </div>
-              )
-            })
-          }
+            todos.length === 0 ? (<div className='text-center text-gray-400 mt-5'>No Tasks</div>) :
+              todos.map((item, id) => {
+                return (
+                  <div key={id}>
+                    <div className='flex items-center'>
+                      <input type="checkbox" name="show" id="show" />
+                      <label htmlFor="show" className='ml-1'>Finshed Todos</label>
+                    </div>
 
+                    <div>
+                      <input type="checkbox" name="show" id="shownot" />
+                      <label htmlFor="show"></label>
+                    </div>
+
+                    <div className='flex items-center'>
+                      <div>
+                        <label htmlFor="show"></label>
+                        <input type="checkbox" name="show" id="shownot" />
+                      </div>
+
+                      <div className='flex justify-between items-center w-full'>
+
+                        <div>{item.todo}</div>
+
+                        <div className='flex gap-2 mt-2'>
+                          <button className='border button bg-gray-200 font-bold p-2 rounded-[10px]' onClick={(e) => editEvent(e, item.id)}><FaEdit /></button>
+                          <button className='border button bg-gray-200 font-bold p-2 rounded-[10px]' onClick={(e) => deleteEvent(e, item.id)}><AiFillDelete /></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })
+          }
         </div >
       </div>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-gray-100 pt-6 pb-6 text-center text-2xl mx-auto font-[24px]'>
+            <div>
+                <div className="logo part1">&copy; 2025 Ansh Tyagi. All rights reserved.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
