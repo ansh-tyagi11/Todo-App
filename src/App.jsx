142b598ac5f9e1804d16f320a51f5e6d522ebf64
Ansh Tyagi <officialanshtyagi112@gmail.com>
2025-09-18 23:32:44 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
-  const [third, setThird] = useState(false)
+  const [completed, setCompleted] = useState(false)
   const focus = useRef(null)
 
   useEffect(() => {
@@ -27,7 +27,7 @@ function App() {
   }, [])
 
   const add = () => {
-    setTodos([...todos, { id: uuidv4(), todo }])
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
     setTodo("")
     saveToLS()
   }
@@ -45,40 +45,47 @@ function App() {
     })
     deleteEvent(edit, id)
     setTodo(edit[0].todo)
-    saveToLS()
+    // saveToLS()
+  }
+
+  const isCompleted = (_, id) => {
+    let newTodos = todos.map(item => {
+      if (item.id === id) {
+        return { ...item, isCompleted: !item.isCompleted }
+      }
+      return item
+    })
+    setTodos(newTodos)
+  }
+
+  const handelChange = (e) => {
+    setCompleted(!completed)
   }
 
   return (
     <>
       <Navbar />
-      <div className='bg-gray-100 flex justify-center min-h-[75vh] mx-auto pb-3'>
+      <div className='bg-gray-100 flex justify-center min-h-[82vh] mx-auto pb-3'>
         <div className="container bg-white w-[600px] rounded-3xl h-auto shadow py-3.5 px-3.5">
 
           <div className='button flex justify-between items-center'>
             <input onChange={(e) => setTodo(e.target.value)} value={todo} ref={focus} className='border border-gray-300 rounded-[8px] w-[460px] h-[40px] text-[16px] px-1' type="text" name='text' placeholder='Add a new task...' />
             <button onClick={add} className='w-[80px] h-[40px] bg-blue-500 rounded-xl text-white text-bold'>Add</button>
           </div>
-          <h2 className='font-bold mt-[25px]'>My Todo's</h2>
-
+          <h2 className='font-bold mt-[25px] flex justify-center'>My Todo's</h2>
+          <div className='flex items-center'>
+            <input type="checkbox" name="show" id="show" onChange={handelChange} checked={completed} />
+            <label htmlFor="show" className='ml-1'>Finshed Todos</label>
+          </div>
           {
             todos.length === 0 ? (<div className='text-center text-gray-400 mt-5'>No Tasks</div>) :
               todos.map((item, id) => {
                 return (
                   <div key={id}>
                     <div className='flex items-center'>
-                      <input type="checkbox" name="show" id="show" />
-                      <label htmlFor="show" className='ml-1'>Finshed Todos</label>
-                    </div>
-
-                    <div>
-                      <input type="checkbox" name="show" id="shownot" />
-                      <label htmlFor="show"></label>
-                    </div>
-
-                    <div className='flex items-center'>
                       <div>
                         <label htmlFor="show"></label>
-                        <input type="checkbox" name="show" id="shownot" />
+                        <input type="checkbox" name="show" id="shownot" onChange={(e) => isCompleted(e, item.id)} checked={item.isCompleted} />
                       </div>
 
                       <div className='flex justify-between items-center w-full'>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-gray-100 pt-6 pb-6 text-center text-2xl mx-auto font-[24px]'>
+        <div className='bg-gray-100 pt-6 pb-6 text-center mx-auto font-[24px]'>
             <div>
                 <div className="logo part1">&copy; 2025 Ansh Tyagi. All rights reserved.</div>
             </div>
